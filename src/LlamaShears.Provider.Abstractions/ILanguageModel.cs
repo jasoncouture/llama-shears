460f4ceb1341a63d709b2e4648e8584b67f4c263
Jason Couture <jasonc@alertr.info>
2026-05-06 13:38:00 -0400

    refactor(provider): split abstractions into one type per file (ModelConfiguration, ILanguageModel, SupportedInputType, ModelInfo, IProviderFactory)

---
+++ src/LlamaShears.Provider.Abstractions/ILanguageModel.cs
@@ -0,0 +1,9 @@
+namespace LlamaShears.Provider.Abstractions;
+
+/// <summary>
+/// Base interface for all language models.
+/// </summary>
+public interface ILanguageModel
+{
+    // The core interface for all model interactions will be defined here.
+}
